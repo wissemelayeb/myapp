98bb585e42f4c8550f2187478785c0b6a7fe5076
Wissem Elayeb <elayebwissem24@gmail.com>
2023-05-02 13:39:09 +0100

    first commit

--- src/component/Profile/ProfilePhoto.js
+++ src/component/Profile/ProfilePhoto.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import MESSI from '../../Image/MESSI.jpg'
+import Form from 'react-bootstrap/Form';
 const ProfilePhoto = () => {
 return (
 <div style = {{ display : 'flex', justifyContent : 'center', marginTop : '30px'}}>
@@ -15,7 +16,26 @@ est un footballeur international argentin jouant au poste d'attaquant au Paris S
 Septuple vainqueur du Ballon d'or, un record, 
 il est considéré comme l'un des meilleurs joueurs de football toutes générations confondues.
 </Card.Text>
-<Button variant="primary">Go somewhere</Button>
+<Form>
+<Form.Group className="mb-3" controlId="formBasicEmail">
+<Form.Label>Email address</Form.Label>
+<Form.Control type="email" placeholder="Enter email" />
+<Form.Text className="text-muted">
+We'll never share your email with anyone else.
+</Form.Text>
+</Form.Group>
+
+<Form.Group className="mb-3" controlId="formBasicPassword">
+<Form.Label>Password</Form.Label>
+<Form.Control type="password" placeholder="Password" />
+</Form.Group>
+<Form.Group className="mb-3" controlId="formBasicCheckbox">
+<Form.Check type="checkbox" label="Check me out" />
+</Form.Group>
+<Button variant="primary" type="submit">
+Submit
+</Button>
+</Form>
 </Card.Body>
 </Card>
 </div>
